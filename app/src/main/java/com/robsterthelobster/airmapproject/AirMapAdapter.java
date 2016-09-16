6cea8729a35905bf6ec5eaa20b9df771730b776a
Robin Chen <robin3chen@gmail.com>
2016-09-15 23:13:22 -0700

    Remade repo, now public
    Ignoring api config.
    Minor fixes from submission.

---
+++ app/src/main/java/com/robsterthelobster/airmapproject/AirMapAdapter.java
@@ -0,0 +1,99 @@
+package com.robsterthelobster.airmapproject;
+
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.airmap.airmapsdk.models.status.AirMapStatus;
+import com.airmap.airmapsdk.models.status.AirMapStatusAdvisory;
+import com.airmap.airmapsdk.models.status.AirMapStatusWeather;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+
+/**
+ * Created by robin on 9/15/2016.
+ */
+public class AirMapAdapter extends RecyclerView.Adapter<AirMapAdapter.ViewHolder>{
+
+    private final List<AirMapStatus> mValues;
+
+    public AirMapAdapter(List<AirMapStatus> mValues) {
+        this.mValues = mValues;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.card_view, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.mItem = mValues.get(position);
+        int color = 0;
+        String safetyText = "No data.";
+        switch(holder.mItem.getAdvisoryColor()){
+            case Red:
+                color = ContextCompat.getColor(holder.mView.getContext(), R.color.red);
+                safetyText = "Unsafe or unknown. Do not fly!";
+                break;
+            case Green:
+                color = ContextCompat.getColor(holder.mView.getContext(), R.color.green);
+
+                safetyText = "No known issues.";
+                break;
+            case Yellow:
+                color = ContextCompat.getColor(holder.mView.getContext(), R.color.yellow);
+                safetyText = "Action required to fly.";
+                break;
+        }
+        holder.cardView.setBackgroundColor(color);
+        holder.safetyText.setText(safetyText);
+
+        String advisoryText = "";
+        if(holder.mItem.getAdvisories().size() == 0){
+            advisoryText = "No advisory nearby.";
+        }else{
+            for(AirMapStatusAdvisory advisory : holder.mItem.getAdvisories()){
+                advisoryText += "\n"+advisory.toString();
+            }
+        }
+
+        holder.advisoryText.setText(advisoryText);
+
+        AirMapStatusWeather weather = holder.mItem.getWeather();
+        String weatherString = weather.getTemperature() + " degrees, " + weather.getCondition();
+        holder.weatherText.setText(weatherString);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mValues.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        @BindView(R.id.safety_text) TextView safetyText;
+        @BindView(R.id.advisory_text) TextView advisoryText;
+        @BindView(R.id.weather_text) TextView weatherText;
+        @BindView(R.id.card_view) CardView cardView;
+
+        public final View mView;
+        public AirMapStatus mItem;
+
+        public ViewHolder(View view) {
+            super(view);
+            mView = view;
+            ButterKnife.bind(this, view);
+        }
+    }
+}
---
+++ app/src/main/java/com/robsterthelobster/airmapproject/MainActivity.java
@@ -0,0 +1,215 @@
+package com.robsterthelobster.airmapproject;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+
+import com.airmap.airmapsdk.AirMapException;
+import com.airmap.airmapsdk.models.Coordinate;
+import com.airmap.airmapsdk.models.status.AirMapStatus;
+import com.airmap.airmapsdk.networking.callbacks.AirMapCallback;
+import com.airmap.airmapsdk.networking.services.AirMap;
+import com.airmap.airmapsdk.networking.services.MappingService.AirMapAirspaceType;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
+
+    private final String TAG = MainActivity.class.getSimpleName();
+    private final int LOCATION_REQUEST_CODE = 1;
+
+    @BindView(R.id.recycler_view) RecyclerView mRecyclerView;
+    @BindView(R.id.fab) FloatingActionButton fab;
+    @BindView(R.id.toolbar) Toolbar toolbar;
+
+    private GoogleApiClient mGoogleApiClient;
+    protected LocationRequest mLocationRequest;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    AirMapAdapter mAdapter;
+    List<AirMapStatus> mData;
+    Location mLocation;
+
+    boolean mRequestingLocationUpdates = true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        AirMap.init(MainActivity.this);
+
+        setSupportActionBar(toolbar);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(mLocation != null){
+                    callAirMap(mLocation.getLatitude(), mLocation.getLongitude());
+                    Snackbar.make(view, "Getting flight status", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                }else {
+                    Snackbar.make(view, "Location not available", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                }
+            }
+        });
+
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+
+        mData = new ArrayList<>();
+        mAdapter = new AirMapAdapter(mData);
+
+        mLayoutManager = new LinearLayoutManager(this);
+        ((LinearLayoutManager) mLayoutManager).setStackFromEnd(true);
+        ((LinearLayoutManager) mLayoutManager).setReverseLayout(true);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(mAdapter);
+    }
+
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
+
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopLocationUpdates();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (mGoogleApiClient.isConnected() && !mRequestingLocationUpdates) {
+            startLocationUpdates();
+        }
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        startLocationUpdates();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mLocation = location;
+        callAirMap(location.getLatitude(), location.getLongitude());
+    }
+
+    protected void stopLocationUpdates() {
+        LocationServices.FusedLocationApi.removeLocationUpdates(
+                mGoogleApiClient, this);
+    }
+
+    protected void startLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(
+                    this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    LOCATION_REQUEST_CODE);
+            return;
+        }
+        mLocationRequest = LocationRequest.create();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(10000); // get location every 10s
+
+        LocationServices.FusedLocationApi.requestLocationUpdates(
+                mGoogleApiClient, mLocationRequest, this);
+    }
+
+    private void callAirMap(double lat, double lon){
+        Coordinate coordinate = new Coordinate(lat, lon);
+        boolean showWeather = true; //Return weather information
+        Date date = new Date(); //The current date/time
+        double buffer = 1000; //1000 meters
+
+        // All the types
+        List<AirMapAirspaceType> types = Arrays.asList(
+                AirMapAirspaceType.Airport,
+                AirMapAirspaceType.ControlledAirspace,
+                AirMapAirspaceType.SpecialUse,
+                AirMapAirspaceType.TFR,
+                AirMapAirspaceType.Wildfires,
+                AirMapAirspaceType.Park,
+                AirMapAirspaceType.PowerPlant,
+                AirMapAirspaceType.School,
+                AirMapAirspaceType.SpecialUse,
+                AirMapAirspaceType.Heliport,
+                AirMapAirspaceType.Hospitals);
+
+        AirMap.checkCoordinate(coordinate, buffer, types, null, showWeather, date, new AirMapCallback<AirMapStatus>() {
+            @Override
+            public void onSuccess(final AirMapStatus response) {
+                updateUI(response);
+            }
+
+            @Override
+            public void onError(final AirMapException e) {
+                Log.v(TAG, "Error getting Status");
+                e.printStackTrace();
+            }
+        });
+    }
+
+    private void updateUI(final AirMapStatus airMapStatus) {
+        this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mData.add(airMapStatus);
+                mAdapter.notifyItemChanged(mData.size() - 1);
+                mRecyclerView.smoothScrollToPosition(mData.size() - 1);
+            }
+        });
+    }
+}
